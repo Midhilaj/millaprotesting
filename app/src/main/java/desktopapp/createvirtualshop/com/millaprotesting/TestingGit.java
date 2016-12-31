abf08238fb198a513021263dfb319541885fa577
midhilaj <april12m4a@gmail.com>
2017-01-01 02:14:49 +0530

    checking commit message

---
+++ app/src/main/java/desktopapp/createvirtualshop/com/millaprotesting/TestingGit.java
@@ -0,0 +1,18 @@
+package desktopapp.createvirtualshop.com.millaprotesting;
+
+/**
+ * Created by midhilaj on 1/1/17.
+ */
+
+public class TestingGit {
+    String mEmialid;
+
+    public String getmEmialid() {
+        return mEmialid;
+    }
+
+    public void setmEmialid(String mEmialid) {
+        this.mEmialid = mEmialid;
+    }
+}
+
